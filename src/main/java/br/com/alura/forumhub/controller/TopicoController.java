819f1efc46dbc4bb8967c4208547820f7d83af29
cristiellesreis <cristielle.santos94@gmail.com>
2025-08-08 16:16:01 -0300

    feat: tratando erros

--- src/main/java/br/com/alura/forumhub/controller/TopicoController.java
+++ src/main/java/br/com/alura/forumhub/controller/TopicoController.java
@@ -48,11 +48,11 @@ public class TopicoController {
         return ResponseEntity.ok(page);
     }
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity detalhar(@PathVariable Long id) {
-//        var topico = repository.getReferenceById(id);
-//        return ResponseEntity.ok(new DadosDetalhamentoTopico(topico));
-//    }
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var topico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoTopico(topico));
+    }
 
     @PutMapping("/{id}")
     @Transactional
