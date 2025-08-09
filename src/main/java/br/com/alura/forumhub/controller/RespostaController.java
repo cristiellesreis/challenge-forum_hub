8a2c479e44c46bd470f5caf173c6ea0a2bf5a864
cristiellesreis <cristielle.santos94@gmail.com>
2025-08-09 15:31:14 -0300

    feat: cria listagem de resposta filtrando por tópico

--- src/main/java/br/com/alura/forumhub/controller/RespostaController.java
+++ src/main/java/br/com/alura/forumhub/controller/RespostaController.java
@@ -1,19 +1,18 @@
 package br.com.alura.forumhub.controller;
 
-import br.com.alura.forumhub.domain.resposta.DadosCadastroResposta;
-import br.com.alura.forumhub.domain.resposta.DadosDetalhamentoResposta;
-import br.com.alura.forumhub.domain.resposta.Resposta;
-import br.com.alura.forumhub.domain.resposta.RespostaRepository;
+import br.com.alura.forumhub.domain.curso.DadosDetalhamentoCurso;
+import br.com.alura.forumhub.domain.curso.DadosListagemCurso;
+import br.com.alura.forumhub.domain.resposta.*;
 import br.com.alura.forumhub.domain.topico.TopicoRepository;
 import br.com.alura.forumhub.domain.usuario.UsuarioRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -42,4 +41,12 @@ public class RespostaController {
 
         return ResponseEntity.created(uri).body(new DadosDetalhamentoResposta(resposta));
     }
+
+    @GetMapping("/topico/{id}")
+    public ResponseEntity<Page<DadosListagemResposta>> listar(@PageableDefault(size = 10, sort = {"data"}) Pageable paginacao) {
+        var page = repository.findAll(paginacao).map(DadosListagemResposta::new);
+        return ResponseEntity.ok(page);
+    }
+
+
 }
--- src/main/java/br/com/alura/forumhub/domain/resposta/RespostaRepository.java
+++ src/main/java/br/com/alura/forumhub/domain/resposta/RespostaRepository.java
@@ -1,6 +1,10 @@
 package br.com.alura.forumhub.domain.resposta;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RespostaRepository extends JpaRepository<Resposta, Long> {
+
+    Page<Resposta> findByTopicoId(Long topicoId, Pageable pageable);
 }
