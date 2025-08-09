7c31ae689dfbc47ee9281a1ea317a5a61bee8a71
cristiellesreis <cristielle.santos94@gmail.com>
2025-08-09 19:40:06 -0300

    feat: incluir perfis na resposta dos DTOs de listagem e detalhamento

--- src/main/java/br/com/alura/forumhub/domain/usuario/DadosDetalhamentoUsuario.java
+++ src/main/java/br/com/alura/forumhub/domain/usuario/DadosDetalhamentoUsuario.java
@@ -1,10 +1,20 @@
 package br.com.alura.forumhub.domain.usuario;
 
+import br.com.alura.forumhub.domain.perfil.Perfil;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public record DadosDetalhamentoUsuario(Long id,
-                                       String nome) {
+                                       String nome,
+                                       String email,
+                                       Set<String> perfis) {
 
     public DadosDetalhamentoUsuario(Usuario usuario) {
 
-        this(usuario.getId(), usuario.getNome());
+        this(usuario.getId(), usuario.getNome(), usuario.getEmail(),
+                usuario.getPerfis().stream()
+                        .map(Perfil::getNome)
+                        .collect(Collectors.toSet()));
     }
 }
--- src/main/java/br/com/alura/forumhub/domain/usuario/DadosListagemUsuario.java
+++ src/main/java/br/com/alura/forumhub/domain/usuario/DadosListagemUsuario.java
@@ -1,8 +1,15 @@
 package br.com.alura.forumhub.domain.usuario;
 
-public record DadosListagemUsuario(Long id, String nome) {
+import br.com.alura.forumhub.domain.perfil.Perfil;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record DadosListagemUsuario(Long id, String nome, Set<String> perfis) {
 
     public DadosListagemUsuario(Usuario usuario){
-        this(usuario.getId(), usuario.getNome());
+        this(usuario.getId(), usuario.getNome(), usuario.getPerfis().stream()
+                .map(Perfil::getNome)
+                .collect(Collectors.toSet()));
     }
 }
