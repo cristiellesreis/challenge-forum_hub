2b0aa34ba07a565da85b9cb326e625fce60628ff
cristiellesreis <cristielle.santos94@gmail.com>
2025-08-11 17:52:45 -0300

    feat: gera token de acesso

--- src/main/java/br/com/alura/forumhub/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forumhub/controller/AutenticacaoController.java
@@ -1,6 +1,9 @@
 package br.com.alura.forumhub.controller;
 
 import br.com.alura.forumhub.domain.usuario.DadosAutenticacao;
+import br.com.alura.forumhub.domain.usuario.Usuario;
+import br.com.alura.forumhub.infra.security.DadosTokenJWT;
+import br.com.alura.forumhub.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +21,18 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
 
     }
 }
