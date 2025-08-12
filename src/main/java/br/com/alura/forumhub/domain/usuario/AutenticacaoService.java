807330719d2da699fb136757b5f1e322e4f79bb5
cristiellesreis <cristielle.santos94@gmail.com>
2025-08-12 17:11:13 -0300

    feat: implementando controle de acesso

--- src/main/java/br/com/alura/forumhub/domain/usuario/AutenticacaoService.java
+++ src/main/java/br/com/alura/forumhub/domain/usuario/AutenticacaoService.java
@@ -14,6 +14,7 @@ public class AutenticacaoService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByEmail(username);
+        return repository.findByEmailWithPerfis(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
     }
 }
--- src/main/java/br/com/alura/forumhub/domain/usuario/UsuarioRepository.java
+++ src/main/java/br/com/alura/forumhub/domain/usuario/UsuarioRepository.java
@@ -1,11 +1,17 @@
 package br.com.alura.forumhub.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     boolean existsByEmail(String email);
 
-    UserDetails findByEmail(String email);
+    @Query("SELECT u FROM Usuario u JOIN FETCH u.perfis WHERE u.email = :email")
+    Optional<Usuario> findByEmailWithPerfis(@Param("email") String email);
+
 }
